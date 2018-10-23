93aebd43ab435af1bd07c6437d95cc074d86ded3
JingboWang1997 <wjbjimmy@gmail.com>
2018-10-23 08:40:59 -0400

    PS3 almost

--- Problem Set/PS3/main.py
+++ Problem Set/PS3/main.py
@@ -3,6 +3,7 @@ from quantizeHSV import quantizeHSV
 from computeQuantizationError import computeQuantizationError
 from getHueHists import getHueHists
 from detectCircles import detectCircles
+from detectAllCircles import detectAllCircles
 
 import matplotlib.pyplot as plt
 
@@ -33,8 +34,36 @@ fishImg = plt.imread('./fish.jpg')
 # plt.show(histClustered)
 
 img = plt.imread('./egg.jpg')
-radius = 5
-centers = detectCircles(img, radius, 1)
+plt.imshow(img)
+plt.show()
+radius = 108
+# with gradient
+# centers = detectCircles(img.copy(), radius, 1)
+# print centers
+# # plot circles
+# fig = plt.figure()
+# plt.imshow(img)
+# circle = []
+# for center in centers:
+#     fig.add_subplot(111).add_artist(plt.Circle(center,radius,color=(1,0,0),fill=False))
+# plt.show()
+# without gradient
+# centers = detectCircles(img.copy(), radius, 0)
+# print centers
+# # plot circles
+# fig = plt.figure()
+# plt.imshow(img)
+# circle = []
+# for center in centers:
+#     fig.add_subplot(111).add_artist(plt.Circle(center,radius,color=(1,0,0),fill=False))
+# plt.show()
+
+centers = detectAllCircles(img.copy())
 print centers
-centers = detectCircles(img, radius, 0)
-print centers
+# plot circles
+fig = plt.figure()
+plt.imshow(img)
+circle = []
+for center in centers:
+    fig.add_subplot(111).add_artist(plt.Circle((center[0],center[1]),center[2],color=(1,0,0),fill=False))
+plt.show()
